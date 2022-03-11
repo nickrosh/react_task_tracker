26f3d8e125c54f1bd794ae8674c8cb949f92a3b6
Nick Roshdieh <nickroshd@gmail.com>
2022-03-11 00:04:47 -0500

    each individual task is now a seperate Task component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from "react"
 
 const App = () => {
@@ -24,10 +25,22 @@ const App = () => {
     },
   ])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}
+       onToggle={toggleReminder} />) : 
+      ('No Tasks to Show')}
     </div>
   );
 }
